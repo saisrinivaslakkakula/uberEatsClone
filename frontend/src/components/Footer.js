b099dccebb6beeeebb9dc067ce235933045a4094
saisrinivaslakkakula <38246768+saisrinivaslakkakula@users.noreply.github.com>
2021-09-10 02:48:53 -0700

    After Footer Component V2

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,34 +10,49 @@ const Footer = () => {
                             <h2>Uber <span>Eats</span></h2>
                         </div>
                     </div>
-                    <div className="col-md-3"> 
-                    <ul className="nobullets">
-                        <li>
-                            <a href="#"> Get Help</a>
-                        </li>
-                        <li>
-                        <a href="#">  Buy gift cards</a>
-                        </li>
-                        <li>
-                        <a href="#">  Add your restraunt</a>
-                        </li>
-                    </ul>
-                     </div>
-                    <div className="col-md-3"> 
-                    <ul className="nobullets">
-                        <li>
-                            <a href="#"> Restaraunts near me</a>
-                        </li>
-                        <li>
-                        <a href="#">  View all cities</a>
-                        </li>
-                        <li>
-                        <a href="#">  About Uber Eats</a>
-                        </li>
-                    </ul>
+                    <div className="col-md-3">
+                        <ul className="nobullets">
+                            <li>
+                                <a href="#"> Get Help</a>
+                            </li>
+                            <li>
+                                <a href="#">  Buy gift cards</a>
+                            </li>
+                            <li>
+                                <a href="#">  Add your restraunt</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-3">
+                        <ul className="nobullets">
+                            <li>
+                                <a href="#"> Restaraunts near me</a>
+                            </li>
+                            <li>
+                                <a href="#">  View all cities</a>
+                            </li>
+                            <li>
+                                <a href="#">  About Uber Eats</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
+                <hr></hr>
+                <div className="row">
+                    <div className="col-md-6"></div>
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="col-md-3"><a href="#">  Privacy Policy</a></div>
+                            <div className="col-md-3"> <a href="#">Terms</a></div>
+                            <div className="col-md-3"><a href="#">Pricing</a></div>
+                            <div className="col-md-3"><a href="#">Do not Sell my Info</a></div>
+                        </div>
 
+                    </div>
+                </div>
+                <div className="row">
+                    <p> This Web Application is a cloned version of Uber Eats, developed as part of School project. The Design Copyrights belong to Uber Technologies Inc. &copy; </p>
+                </div>
 
             </div>
         </footer>
