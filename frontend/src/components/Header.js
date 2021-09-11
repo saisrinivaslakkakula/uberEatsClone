67b4e05cec27c78c79894f7c6460b8e00457beb9
saisrinivaslakkakula <38246768+saisrinivaslakkakula@users.noreply.github.com>
2021-09-11 01:06:13 -0700

    nav bar added Sidebar Dots

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,15 @@
 import React from 'react'
-
+import * as FaIcons from 'react-icons/fa'
 const Header = () => {
     return (
         <header>
-            <div className="container">
+            <div className="container header">
+                <div className='menu-bars'> <FaIcons.FaBars/></div>
                 <div className="logo">
                     <h1>Uber <span>Eats</span></h1>
                 </div>
-                <div className="currentDetails">
-                    <div className="header-option">
-                        <i data-feather="map-pin"></i>
-                        <span> London</span>
-                    </div>
-                    <div className="header-option">
-                        <i data-feather="clock"></i>
-                        <span>Deliver Now</span>
-                    </div>
-                </div>
                 <div className="searchBar">
-                    <div className="header-option">
-                        <i data-feather="search"></i>
-                        <span>Search</span>
-                    </div>
+                    
                     <div className="header-option">
                         <span>Sign in</span>
                     </div>
---
+++ frontend/src/components/SideBarData.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBarData = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SideBarData
