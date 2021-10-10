8887bc80c13d6d5b0361d0321c60834d3f3ef317
saisrinivaslakkakula <38246768+saisrinivaslakkakula@users.noreply.github.com>
2021-10-10 14:23:26 -0700

    small bug fixes v2

--- backend/dbCon.js
+++ backend/dbCon.js
@@ -3,7 +3,6 @@ const env = require('dotenv')
 env.config()
 const db = mysql.createConnection({
     host     : process.env.DB_URI,
-    port:3306,
       user     : 'admin',
       password : 'mydatabase',
       database:'ubereats',
